8073137a84d0ad49d6ee0eb9cd5efb5df6d45e35
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-05-17 20:57:55 -0300

    Add rate limiter middleware

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+app.use(limiter);
+
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
