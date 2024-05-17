f84926a04724e0ff3a047129d37e5d496534264d
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-05-17 14:51:36 -0300

    Add list of categories and category items

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,12 +1,34 @@
 const Category = require("../models/Category");
+const Item = require("../models/Item");
 const asyncHandler = require("express-async-handler");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Category List");
+    const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+    res.render("category_list", {
+        title: "Category List",
+        category_list: allCategories,
+    });
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) => {
-    res.send(`NOT IMPLEMENTED: Category Detail: ${req.params.id}`);
+
+    const [category, itemsInCategory] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({ category: req.params.id }, "name description").exec(),
+    ]);
+
+    if (category == null) {
+        const err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("category_detail", {
+        title: "Category Detail",
+        category: category,
+        category_items: itemsInCategory,
+    });
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
