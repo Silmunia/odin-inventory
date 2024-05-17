80a4d43c08a6bead183097b37ce999300b25a274
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-05-17 20:35:27 -0300

    Add option of updating Items

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -104,12 +104,80 @@ exports.item_create_post = [
 ];
 
 exports.item_update_get = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update GET");
+    const item = await Item.findById(req.params.id).exec();
+
+    if (item === null) {
+        const error = new Error("Item not found");
+        error.status = 404;
+        return next(error);
+    }
+
+    const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+    res.render("item_form", {
+        title: "Update Item",
+        categories: allCategories,
+        item: item,
+    });
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Item update POST");
-});
+exports.item_update_post = [
+    body("name", "Item name must contain at least 1 character")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    body("description", "Item description must contain at least 3 characters")
+        .trim()
+        .isLength({ min: 3 })
+        .escape(),
+
+    body("price", "Item price must be a number bigger than 0")
+        .trim()
+        .optional({ checkFalsy: true })
+        .isNumeric()
+        .escape(),
+
+    body("stock", "Item stock must be a number equal to or bigger than 0")
+        .trim()
+        .optional({ checkFalsy: true })
+        .isNumeric()
+        .escape(),
+
+    body("category", "Item category must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const item = new Item({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            stock: req.body.stock,
+            category: req.body.category,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+            res.render("item_form", {
+                title: "Update Item",
+                categories: allCategories,
+                item: item,
+                errors: errors.array(),
+            });
+            return;
+        }
+
+        const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {});
+
+        res.redirect(updatedItem.url);
+    }),
+];
 
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
     res.send("NOT IMPLEMENTED: Item delete GET");
